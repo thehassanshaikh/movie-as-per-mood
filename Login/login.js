b844d63b8eb0efa4ced577021f52fbd0fc666290
thehassanshaikh <thehasanshaikh@gmail.com>
2023-05-11 19:08:02 +0530

    added button link

---
+++ Login/login.js
@@ -0,0 +1,12 @@
+
+
+
+
+function handleLogin(e) {
+
+    e.preventDefault()
+    const userName = document.querySelector('#usr').value;
+    const password = document.querySelector('#pwd').value;
+
+    console.log(userName,password)
+}
